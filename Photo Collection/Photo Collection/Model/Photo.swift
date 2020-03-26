fa0974bee0637f7d41ccb58645a15447e5ae97a9
Wesley White <wesleyrwhite86@gmail.com>
2020-03-26 09:42:50 -1000

    Created Photo Collection project, added Photo model

---
+++ Photo Collection/Photo Collection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Wesley Ryan on 3/26/20.
+//  Copyright © 2020 Wesley Ryan. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
